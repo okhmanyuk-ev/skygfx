0d2393b557ac9b7852e53e22fc51d14b39fa9d71
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2022-06-03 11:56:24 +1000

    hello glfw window

---
+++ examples/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	if (!glfwInit())
+		return -1;
+
+	int width = 800;
+	int height = 600;
+
+	auto window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
+
+	int count = 0;
+	auto monitors = glfwGetMonitors(&count);
+
+	auto video_mode = glfwGetVideoMode(monitors[0]);
+
+	auto window_pos_x = (video_mode->width / 2) - (width / 2);
+	auto window_pos_y = (video_mode->height / 2) - (height / 2);
+
+	glfwSetWindowPos(window, window_pos_x, window_pos_y);
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
