14ea955cb3da47d038756b8e316ca8de9bbb5aac
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2022-06-20 13:38:58 +1000

    add light example

---
+++ examples/light/main.cpp
@@ -0,0 +1,237 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+#include <skygfx/skygfx.h>
+#include "../utils/utils.h"
+
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>	
+
+static std::string vertex_shader_code = R"(
+#version 450 core
+
+layout(location = POSITION_LOCATION) in vec3 aPosition;
+layout(location = TEXCOORD_LOCATION) in vec2 aTexCoord;
+layout(location = NORMAL_LOCATION) in vec3 aNormal;
+
+layout(binding = 1) uniform _ubo
+{
+	mat4 projection;
+	mat4 view;
+	mat4 model;
+} ubo;
+
+layout(location = 0) out struct { vec3 Position; vec3 Normal; vec2 TexCoord; } Out;
+out gl_PerVertex { vec4 gl_Position; };
+
+void main()
+{
+	Out.Position = vec3(ubo.model * vec4(aPosition, 1.0));
+	Out.Normal = vec3(ubo.model * vec4(aNormal, 1.0));
+	Out.TexCoord = aTexCoord;
+#ifdef FLIP_TEXCOORD_Y
+	Out.TexCoord.y = 1.0 - Out.TexCoord.y;
+#endif
+	gl_Position = ubo.projection * ubo.view * ubo.model * vec4(aPosition, 1.0);
+})";
+
+static std::string fragment_shader_code = R"(
+#version 450 core
+
+layout(binding = 2) uniform _light
+{
+	vec3 direction;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	vec3 eye_position;
+	float shininess;
+} light;
+
+layout(location = 0) out vec4 result;
+layout(location = 0) in struct { vec3 Position; vec3 Normal; vec2 TexCoord; } In;
+layout(binding = 0) uniform sampler2D sTexture;
+
+void main() 
+{ 
+	result = texture(sTexture, In.TexCoord);
+
+	vec3 normal = normalize(In.Normal);
+	vec3 view_dir = normalize(light.eye_position - In.Position);
+	vec3 light_dir = normalize(light.direction);
+
+	float diff = max(dot(normal, -light_dir), 0.0);
+	vec3 reflectDir = reflect(light_dir, normal);
+	float spec = pow(max(dot(view_dir, reflectDir), 0.0), light.shininess);
+
+	vec3 intensity = light.ambient + (light.diffuse * diff) + (light.specular * spec);
+
+	result *= vec4(intensity, 1.0);
+})";
+
+using Vertex = skygfx::Vertex::PositionTextureNormal;
+
+const std::vector<Vertex> vertices = {
+	/* front */
+	/* 0  */ { { -1.0f,  1.0f,  1.0f }, { 0.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } },
+	/* 1  */ { {  1.0f,  1.0f,  1.0f }, { 1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } },
+	/* 2  */ { { -1.0f, -1.0f,  1.0f }, { 0.0f, 1.0f }, { 0.0f, 0.0f, 1.0f } },
+	/* 3  */ { {  1.0f, -1.0f,  1.0f }, { 1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f } },
+
+	/* top */
+	/* 4  */ { { -1.0f,  1.0f,  1.0f }, { 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
+	/* 5  */ { { -1.0f,  1.0f, -1.0f }, { 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f } },
+	/* 6  */ { {  1.0f,  1.0f,  1.0f }, { 1.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
+	/* 7  */ { {  1.0f,  1.0f, -1.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f, 0.0f } },
+
+	/* left */
+	/* 8  */ { { -1.0f,  1.0f, -1.0f }, { 0.0f, 0.0f }, { -1.0f, 0.0f, 0.0f } },
+	/* 9  */ { { -1.0f,  1.0f,  1.0f }, { 1.0f, 0.0f }, { -1.0f, 0.0f, 0.0f } },
+	/* 10 */ { { -1.0f, -1.0f, -1.0f }, { 0.0f, 1.0f }, { -1.0f, 0.0f, 0.0f } },
+	/* 11 */ { { -1.0f, -1.0f,  1.0f }, { 1.0f, 1.0f }, { -1.0f, 0.0f, 0.0f } },
+
+	/* back */
+	/* 12 */ { { -1.0f,  1.0f, -1.0f }, { 1.0f, 0.0f }, { 0.0f, 0.0f, -1.0f } },
+	/* 13 */ { { -1.0f, -1.0f, -1.0f }, { 1.0f, 1.0f }, { 0.0f, 0.0f, -1.0f } },
+	/* 14 */ { {  1.0f,  1.0f, -1.0f }, { 0.0f, 0.0f }, { 0.0f, 0.0f, -1.0f } },
+	/* 15 */ { {  1.0f, -1.0f, -1.0f }, { 0.0f, 1.0f }, { 0.0f, 0.0f, -1.0f } },
+
+	/* bottom */
+	/* 16 */ { { -1.0f, -1.0f,  1.0f }, { 0.0f, 0.0f }, { 0.0f, -1.0f, 0.0f } },
+	/* 17 */ { {  1.0f, -1.0f,  1.0f }, { 0.0f, 1.0f }, { 0.0f, -1.0f, 0.0f } },
+	/* 18 */ { { -1.0f, -1.0f, -1.0f }, { 1.0f, 0.0f }, { 0.0f, -1.0f, 0.0f } },
+	/* 19 */ { {  1.0f, -1.0f, -1.0f }, { 1.0f, 1.0f }, { 0.0f, -1.0f, 0.0f } },
+
+	/* right */
+	/* 20 */ { { 1.0f, -1.0f, -1.0f }, { 1.0f, 1.0f }, { 1.0f, 0.0f, 0.0f } },
+	/* 21 */ { { 1.0f, -1.0f,  1.0f }, { 0.0f, 1.0f }, { 1.0f, 0.0f, 0.0f } },
+	/* 22 */ { { 1.0f,  1.0f, -1.0f }, { 1.0f, 0.0f }, { 1.0f, 0.0f, 0.0f } },
+	/* 23 */ { { 1.0f,  1.0f,  1.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f, 0.0f } },
+};
+
+static std::vector<uint32_t> indices = {
+	0, 1, 2, 1, 3, 2, // front
+	4, 5, 6, 5, 7, 6, // top
+	8, 9, 10, 9, 11, 10, // left
+	12, 13, 14, 13, 15, 14, // back
+	16, 17, 18, 17, 19, 18, // bottom
+	20, 21, 22, 21, 23, 22, // right
+};
+
+static struct alignas(16) UniformBuffer
+{
+	glm::mat4 projection = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 model = glm::mat4(1.0f);
+} ubo;
+
+static struct alignas(16) Light
+{
+	alignas(16) glm::vec3 direction;
+	alignas(16) glm::vec3 ambient;
+	alignas(16) glm::vec3 diffuse;
+	alignas(16) glm::vec3 specular;
+	alignas(16) glm::vec3 eye_position;
+	float shininess;
+} light;
+
+int main()
+{
+	auto backend_type = utils::ChooseBackendTypeViaConsole();
+
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	uint32_t width = 800;
+	uint32_t height = 600;
+
+	auto window = glfwCreateWindow(width, height, "Light", NULL, NULL);
+
+	int count = 0;
+	auto monitors = glfwGetMonitors(&count);
+
+	auto video_mode = glfwGetVideoMode(monitors[0]);
+
+	auto window_pos_x = (video_mode->width / 2) - (width / 2);
+	auto window_pos_y = (video_mode->height / 2) - (height / 2);
+
+	glfwSetWindowPos(window, window_pos_x, window_pos_y);
+	glfwMakeContextCurrent(window);
+
+	auto win32_window = glfwGetWin32Window(window);
+
+	auto device = skygfx::Device(backend_type, win32_window, width, height);
+	auto shader = skygfx::Shader(Vertex::Layout, vertex_shader_code, fragment_shader_code);
+
+	auto viewport = skygfx::Viewport();
+	viewport.size = { static_cast<float>(width), static_cast<float>(height) };
+
+	int tex_width = 0;
+	int tex_height = 0;
+	void* tex_memory = stbi_load("assets/bricks.png", &tex_width, &tex_height, nullptr, 4); // TODO: this image has 3 channels, we must can load that type of images
+
+	auto texture = skygfx::Texture(tex_width, tex_height, 4/*TODO: no magic numbers should be*/, tex_memory, true);
+
+	const auto yaw = 0.0f;
+	const auto pitch = glm::radians(-25.0f);
+
+	const auto world_up = glm::vec3{ 0.0f, 1.0f, 0.0f };
+	const auto position = glm::vec3{ -500.0f, 200.0f, 0.0f };
+
+	const float fov = 70.0f;
+	const float near_plane = 1.0f;
+	const float far_plane = 8192.0f;
+
+	const auto scale = 100.0f;
+
+	const float sin_yaw = glm::sin(yaw);
+	const float sin_pitch = glm::sin(pitch);
+
+	const float cos_yaw = glm::cos(yaw);
+	const float cos_pitch = glm::cos(pitch);
+
+	const auto front = glm::normalize(glm::vec3(cos_yaw * cos_pitch, sin_pitch, sin_yaw * cos_pitch));
+	const auto right = glm::normalize(glm::cross(front, world_up));
+	const auto up = glm::normalize(glm::cross(right, front));
+
+	ubo.view = glm::lookAtRH(position, position + front, up);
+	ubo.projection = glm::perspectiveFov(fov, (float)width, (float)height, near_plane, far_plane);
+
+	light.eye_position = position;
+	light.ambient = { 0.25f, 0.25f, 0.25f };
+	light.diffuse = { 1.0f, 1.0f, 1.0f };
+	light.specular = { 1.0f, 1.0f, 1.0f };
+	light.direction = { 1.0f, 0.5f, 0.5f };
+	light.shininess = 32.0f;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		auto time = (float)glfwGetTime();
+
+		ubo.model = glm::mat4(1.0f);
+		ubo.model = glm::scale(ubo.model, { scale, scale, scale });
+		ubo.model = glm::rotate(ubo.model, time, { 0.0f, 1.0f, 0.0f });
+
+		device.clear(glm::vec4{ 0.0f, 0.0f, 0.0f, 1.0f });
+		device.setTopology(skygfx::Topology::TriangleList);
+		device.setViewport(viewport);
+		device.setShader(shader);
+		device.setVertexBuffer(vertices);
+		device.setIndexBuffer(indices);
+		device.setUniformBuffer(1, ubo);
+		device.setUniformBuffer(2, light);
+		device.setCullMode(skygfx::CullMode::Back);
+		device.setSampler(skygfx::Sampler::LinearMipmapLinear);
+		device.setTexture(texture);
+		device.drawIndexed(static_cast<uint32_t>(indices.size()));
+		device.present();
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
